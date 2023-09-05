05524bd144bcc74de38a32669108419b2b68156c
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-09-05 18:05:19 +0900

    feat: add nav animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import DubaiClouds from "@/public/Clouds";
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export default function Home() {
   const scrollElement = useRef<any[]>([]);
@@ -14,17 +14,115 @@ export default function Home() {
     }
   }, []);
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <>
-      <nav className="fixed bg-white dark:bg-gray-900">
-        <ul>
-          <li>me</li>
-          <li onClick={() => scrollingTop(0)}>Skills & Project</li>
-          <li onClick={() => scrollingTop(1)}>Testimonials</li>
-        </ul>
+      <nav
+        className={`bg-white dark:bg-gray-900 shadow-lg top-0 fixed w-full flex items-center  justify-end p-4 z-50`}
+      >
+        <div className="hidden items-center space-x-12 lg:flex">
+          <div
+            className="text-lg font-semibold text-gray-800 dark:text-white cursor-pointer"
+            onClick={() => scrollingTop(0)}
+          >
+            me
+          </div>
+          <div
+            className="text-lg font-semibold text-gray-800 dark:text-white cursor-pointer"
+            onClick={() => scrollingTop(1)}
+          >
+            Skills & Project
+          </div>
+          <div
+            className="text-lg font-semibold text-gray-800 dark:text-white cursor-pointer"
+            onClick={() => scrollingTop(2)}
+          >
+            Testimonials
+          </div>
+        </div>
+        <div className="flex items-center space-x-4 lg:hidden w-full">
+          <div className="cursor-pointer flex justify-between w-full">
+            <div
+              className={`lg:flex lg:items-center transition-transform duration-300 ease-in-out transform ${
+                menuOpen
+                  ? "translate-x-full  visible"
+                  : "translate-x-0 invisible"
+              }`}
+            >
+              <div
+                className="text-lg font-semibold text-gray-800 dark:text-white cursor-pointer"
+                onClick={() => scrollingTop(0)}
+              >
+                me
+              </div>
+              <div
+                className="text-lg font-semibold text-gray-800 dark:text-white cursor-pointer"
+                onClick={() => scrollingTop(1)}
+              >
+                Skills & Project
+              </div>
+              <div
+                className="text-lg font-semibold text-gray-800 dark:text-white cursor-pointer"
+                onClick={() => scrollingTop(2)}
+              >
+                Testimonials
+              </div>
+            </div>
+            <button
+              type="button"
+              onClick={toggleMenu}
+              className="text-2xl text-gray-800 dark:text-white "
+            >
+              {menuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
       </nav>
-      <div className="relative max-w-5xl mx-auto ">
-        <header className="pt-20 sm:pt-24 lg:pt-32 flex align-center justify-center">
+
+      <div className=" max-w-5xl mx-auto ">
+        <header
+          className="mt-20 sm:mt-24 lg:mt-32 flex align-center justify-center"
+          ref={(el) => (scrollElement.current[0] = el)}
+        >
           <div className="my-auto">
             <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white pb-5">
               Hi, Im suhyun Han
@@ -33,11 +131,11 @@ export default function Home() {
               A Front-end Developer
             </h1>
           </div>
-          <DubaiClouds />
+          {/* <DubaiClouds /> */}
         </header>
         <section
           className="pt-20 sm:pt-24 lg:pt-32"
-          ref={(el) => (scrollElement.current[0] = el)}
+          ref={(el) => (scrollElement.current[1] = el)}
         >
           <p className="relative text-slate-900 font-bold text-2xl lg:text-5xl tracking-tight text-left dark:text-white pb-5">
             Skills & Projects
@@ -96,7 +194,7 @@ export default function Home() {
         </section>
         <section
           className="pt-20 sm:pt-24 lg:pt-32"
-          ref={(el) => (scrollElement.current[1] = el)}
+          ref={(el) => (scrollElement.current[2] = el)}
         >
           <p className="relative text-slate-900 font-bold text-2xl lg:text-5xl tracking-tight text-left dark:text-white pb-5">
             Testimonials
