9ea8d1162ecc38e6984cd170dd9e1abdea2c322b
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-09-11 16:51:00 +0900

    feat: add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-custom":
+          "linear-gradient(90deg, #1c1566 0%, #047de7 50%,  #1c1566 100%)",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(-20px)" },
+          "100%": { opacity: 1, transform: "translateY(0px)" },
+        },
+        fadeInRight: {
+          "0%": { opacity: 0, transform: "translateX(30px)" },
+          "100%": { opacity: 1, transform: "translateX(0px)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in-out",
+        fadeInRight: "fadeInRight 2s ease-in-out",
       },
     },
   },
   plugins: [],
-}
+};
