00eddab940ca8585634f01ebe4ac3aa360e99a4b
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-09-26 21:11:41 +0900

    feat: add testimonial contents

---
+++ app/constants/testimonial.ts
@@ -0,0 +1,14 @@
+export const Testimonial = [
+  {
+    name: "Regis Bafutwabo",
+    position: "Software Engineer",
+    testimonial:
+      "It was really easy to work with Suhyun because she was able to quickly adapt to new situations and learn on the job. Her willingness to learn and her ability to think on her feet made her a valuable asset to the team.",
+  },
+  {
+    name: "Koo Joyeon",
+    position: "PR Specialist",
+    testimonial:
+      "I have worked with Suhyun on the official website redesign project. Suhyun's greatest strength in taking responsibility for the development tasks on the front-end throughout the website development process was her ability to communicate quickly and clearly. Despite the tight deadlines, she accurately shared every step of the process to create a high-quality product within the given time frame. Thanks to her support, I was able to efficiently carry out my tasks as a designer.\nAnother strength of Suhyun is her attitude of continuously striving to improve as a developer. She genuinely love development and continuously pursue further development studies to create products that can deliver a better user experience. As a colleague working together, I was greatly inspired by her efforts to enhance her work abilities. Additionally, I have often regarded her as a positive team player who shares meaningful insights gained during the product development process with other colleagues.",
+  },
+];
