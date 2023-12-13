0bd0c08491c49b74eeba1a8d548420313558d6cd
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-13 16:33:20 +0900

    bugfix: change ttl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    minimumCacheTTL: 3153600,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
